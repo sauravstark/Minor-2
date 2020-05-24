ba51a1acb1ddcec888a98a5abdf35968ff35c843
Saurav Stark <>
2020-05-24 11:31:59 +0530

    create platforms and move player

--- includes/Game/Scenes/Level.hpp
+++ includes/Game/Scenes/Level.hpp
@@ -12,26 +12,6 @@
 #include "..//Sprites/SpriteSheet.hpp"
 
 #include <vector>
-#include <map>
-#include <cstdlib>
-#include <algorithm>
-
-
-struct BulletStats {
-	float attack;
-	float speed;
-};
-
-struct EnemyStats {
-	EnemyStats(int seed);
-	EnemyStats(const EnemyStats& other);
-	float health;
-	float speed;
-	float cooldown;
-	float attack;
-	int type;
-	void resetCooldown();
-};
 
 class SceneGame : public Scene {
 public:
@@ -43,20 +23,17 @@ private:
 	Input input;
 	std::shared_ptr<GameObject> player;
 	std::shared_ptr<GameObject> background;
-	std::vector<std::pair<std::shared_ptr<GameObject>, EnemyStats>> enemies;
-	std::vector<std::pair<std::shared_ptr<GameObject>, BulletStats>> e_bullets;
-	std::vector<std::pair<std::shared_ptr<GameObject>, std::shared_ptr<GameObject>>> p_bullets;
-	float box_size = 100.0f;
-	float cooldown_time = 0.0f;
+	std::vector<std::shared_ptr<GameObject>> tiles;
+	std::vector<float> tile_heights;
 
-	void spawn_enemy();
-	void fireEnemyBullet(std::pair<std::shared_ptr<GameObject>, EnemyStats> pair);
-	void move_player(float time_step);
-	void update_enemy(float time_step);
-	void updateEnemyBullet(float time_step);
-	void end();
 
-	std::vector<std::shared_ptr<GameObject>> inactive_objects;
+	void createTile(unsigned x, unsigned y, SubSprite type);
+	void updatePlayer(float time_step);
+	void updateEnemies(float time_step);
+
+	float box_size = 50.0f;
+	float y_speed = 0.0f;
+	bool grounded = false;
 };
 
 void SceneGame::onCreate() {
@@ -71,6 +48,139 @@ void SceneGame::onCreate() {
 		
 		auto texture = background->addComponent<Texture>();
 		texture->set(2);
+		texture->setSprite({0.0f, 0.5f, 0.5f, 1.0f});
+	}
+	//tiles
+	{
+		createTile(0, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(0, 1, SpriteSheet::Tile::Brown::centre2);
+		createTile(0, 2, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(3);
+
+		createTile(1, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(1, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(1, 2, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(3);
+
+		createTile(2, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(2, 1, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(2);
+
+		createTile(3, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(3, 1, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(2);
+
+		createTile(4, 0, SpriteSheet::Tile::Brown::centre3);
+		createTile(4, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(4, 2, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(3);
+
+		createTile(5, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(5, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(5, 2, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(3);
+
+		createTile(6, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(6, 1, SpriteSheet::Tile::Brown::centre2);
+		createTile(6, 2, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(3);
+
+		createTile(7, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(7, 1, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(2);
+
+		createTile(8, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(8, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(8, 2, SpriteSheet::Tile::Brown::centre1);
+		createTile(8, 3, SpriteSheet::Tile::Brown::regular_solo);
+		tile_heights.push_back(4);
+
+		createTile(9, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(9, 1, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(2);
+
+		createTile(10, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(10, 1, SpriteSheet::Tile::Brown::centre2);
+		createTile(10, 2, SpriteSheet::Tile::Brown::regular_solo);
+		tile_heights.push_back(3);
+
+		createTile(11, 0, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(1);
+
+		createTile(12, 0, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(1);
+
+		tile_heights.push_back(-1);
+
+		createTile(14, 0, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(1);
+
+		createTile(15, 0, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(1);
+
+		tile_heights.push_back(-1);
+
+		createTile(17, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(17, 1, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(2);
+
+		createTile(18, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(18, 1, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(2);
+
+		createTile(19, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(19, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(19, 2, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(3);
+
+		createTile(20, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(20, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(20, 2, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(3);
+
+		createTile(21, 0, SpriteSheet::Tile::Brown::centre3);
+		createTile(21, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(21, 2, SpriteSheet::Tile::Brown::centre1);
+		createTile(21, 3, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(4);
+
+		createTile(22, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(22, 1, SpriteSheet::Tile::Brown::centre2);
+		createTile(22, 2, SpriteSheet::Tile::Brown::centre1);
+		createTile(22, 3, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(4);
+
+		createTile(23, 0, SpriteSheet::Tile::Brown::centre1);
+		createTile(23, 1, SpriteSheet::Tile::Brown::centre1);
+		createTile(23, 2, SpriteSheet::Tile::Brown::centre1);
+		createTile(23, 3, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(4);
+
+		createTile(24, 0, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(1);
+
+		createTile(25, 0, SpriteSheet::Tile::Brown::regular_middle);
+		tile_heights.push_back(1);
+
+		createTile(26, 0, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(1);
+
+		tile_heights.push_back(-1);
+
+		createTile(28, 0, SpriteSheet::Tile::Brown::regular_left);
+		tile_heights.push_back(1);
+
+		createTile(29, 0, SpriteSheet::Tile::Brown::regular_right);
+		tile_heights.push_back(1);
+
+		tile_heights.push_back(-1);
+		
+		createTile(31, 1, SpriteSheet::Tile::Brown::floating_left);
+		tile_heights.push_back(2);
+	}
+	//enemy
+	{
+		//spawn_enemy();
 	}
 	//player
 	{
@@ -78,16 +188,26 @@ void SceneGame::onCreate() {
 
 		auto transform = player->getComponent<Transform>();
 		transform->setScale(box_size, box_size);
+		transform->setX(-15.5f * box_size);
 
 		auto texture = player->addComponent<Texture>();
 		texture->set(0);
-		//texture->setSprite(SpriteSheet::Player::red_fighter5);
+
+		auto animation = player->addComponent<Animation>();
+
+		auto idle = std::make_shared<FrameSet>(1, Repeat::ALTERNATE);
+		idle->setFrameData(0, Frame(SpriteSheet::Player::Grey::stand));
+
+		auto move = std::make_shared<FrameSet>(3, Repeat::ALTERNATE);
+		move->setFrameData(0, Frame(SpriteSheet::Player::Grey::walk_1));
+		move->setFrameData(1, Frame(SpriteSheet::Player::Grey::walk_2));
+		move->setFrameData(2, Frame(SpriteSheet::Player::Grey::walk_3));
+
+		animation->addAnimation("idle", idle);
+		animation->addAnimation("move", move);
+		animation->setAnimation("idle");
 
 		auto collider = player->addComponent<Collider>();
-	}
-	//enemy
-	{
-		spawn_enemy();
 	}
 }
 
@@ -100,282 +220,88 @@ void SceneGame::captureInput() {
 }
 
 void SceneGame::update(float time_step) {
-	move_player(time_step);
-	update_enemy(time_step);
-	updateEnemyBullet(time_step);
-	cooldown_time -= time_step;
-	if (cooldown_time <= 0.0f)
-		spawn_enemy();
+	updatePlayer(time_step);
+	updateEnemies(time_step);
 }
 
-inline void SceneGame::spawn_enemy() {
-	cooldown_time = 5.0f;
-	int seed = rand();
-	std::shared_ptr<GameObject> new_enemy;
-	if (!inactive_objects.empty()) {
-		new_enemy = inactive_objects[inactive_objects.size() - 1];
-		inactive_objects.pop_back();
-	} else {
-		new_enemy = createObject();
-	}
-	EnemyStats new_stat(seed);
-	enemies.push_back({ new_enemy, new_stat });
+void SceneGame::createTile(unsigned x, unsigned y, SubSprite type) {
+	float s_width = 1600.0f / box_size;
+	float s_height = 900.0f / box_size;
+	float str_x = -(s_width / 2 - 0.5) * box_size;
+	float str_y = -(s_height / 2 - 0.5) * box_size;
 
-	auto transform = new_enemy->getComponent<Transform>();
-	transform->setScale(box_size, box_size);
-	float dir = rand();
-	transform->setPos(-800.0f * cosf(dir), -800.0f * sinf(dir));
-
-	auto texture = new_enemy->addComponent<Texture>();
-	texture->set(0);
-	/*
-	switch (seed % 5)
-	{
-	case 0:
-		texture->setSprite(SpriteSheet::Player::cargoship);
-		transform->setScale(box_size * 0.5f, box_size);
-		break;
-	case 1:
-		texture->setSprite(SpriteSheet::Player::carrier);
-		break;
-	case 2:
-		texture->setSprite(SpriteSheet::Player::destroyer);
-		break;
-	case 3:
-		texture->setSprite(SpriteSheet::Player::cruiser);
-		transform->setScale(box_size * 0.5f, box_size);
-		break;
-	case 4:
-		texture->setSprite(SpriteSheet::Player::shuttle);
-		transform->setScale(box_size * 0.5f, box_size);
-		break;
-	}
-	*/
-}
-
-inline void SceneGame::fireEnemyBullet(std::pair<std::shared_ptr<GameObject>, EnemyStats> pair) {
-	std::shared_ptr<GameObject> new_bullet;
-	if (!inactive_objects.empty()) {
-		new_bullet = inactive_objects[inactive_objects.size() - 1];
-		inactive_objects.pop_back();
-	}
-	else {
-		new_bullet = createObject();
-	}
+	auto tile = createObject();
 	
-	EnemyStats& enemy_stat = pair.second;
-	BulletStats new_stat;
-	new_stat.attack = enemy_stat.attack;
-	new_stat.speed = enemy_stat.speed * 4;
-	e_bullets.push_back({ new_bullet, new_stat });
+	auto transform = tile->addComponent<Transform>();
+	transform->setPos(str_x + box_size * x, str_y + box_size * y);
+	transform->setScale(box_size, box_size);
 
-	auto transform = new_bullet->getComponent<Transform>();
-	transform->setScale(box_size * 0.5f, box_size * 0.5f);
-	auto e_transform = pair.first->getComponent<Transform>();
-	transform->setPos(e_transform->getPosition());
-	transform->setRot(e_transform->getRotation());
-	auto texture = new_bullet->addComponent<Texture>();
+	auto texture = tile->addComponent<Texture>();
 	texture->set(1);
-	/*
-	switch (enemy_stat.type)
-	{
-	case 0:
-		texture->setSprite(SpriteSheet::Bullet::Red::_1);
-		//transform->setScale(box_size * 0.5f, box_size);
-		break;
-	case 1:
-		texture->setSprite(SpriteSheet::Bullet::Red::_2);
-		break;
-	case 2:
-		texture->setSprite(SpriteSheet::Bullet::Red::_3);
-		break;
-	case 3:
-		texture->setSprite(SpriteSheet::Bullet::Red::_5);
-		//transform->setScale(box_size * 0.5f, box_size);
-		break;
-	case 4:
-		texture->setSprite(SpriteSheet::Bullet::Red::_8);
-		//transform->setScale(box_size * 0.5f, box_size);
-		break;
-	}
-	*/
+	texture->setSprite(type);
 }
 
-inline void SceneGame::move_player(float time_step) {
-	float speed = 250.0f;
-	float req_dir = 0.0f;
-	int mov_x = 0, mov_y = 0;
-
-	if (input.isKeyPressed(Input::Key::RIGHT) && !input.isKeyPressed(Input::Key::LEFT)) {
-		mov_x = 1;
-	}
-	else if (input.isKeyPressed(Input::Key::LEFT) && !input.isKeyPressed(Input::Key::RIGHT)) {
-		mov_x = -1;
-	}
-
-	if (input.isKeyPressed(Input::Key::UP) && !input.isKeyPressed(Input::Key::DOWN)) {
-		mov_y = 1;
-	}
-	else if (input.isKeyPressed(Input::Key::DOWN) && !input.isKeyPressed(Input::Key::UP)) {
-		mov_y = -1;
-	}
-
-	if (mov_y == 1) {
-		if (mov_x == 1)
-			req_dir = PI * 1.75f;
-		else if (mov_x == -1)
-			req_dir = PI * 0.25f;
-		else
-			req_dir = 0.0f;
-	}
-	else if (mov_y == -1) {
-		if (mov_x == 1)
-			req_dir = PI * 1.25f;
-		else if (mov_x == -1)
-			req_dir = PI * 0.75f;
-		else
-			req_dir = PI;
-	}
-	else {
-		if (mov_x == 1)
-			req_dir = PI * 1.5f;
-		else if (mov_x == -1)
-			req_dir = PI * 0.5f;
-	}
-
+void SceneGame::updatePlayer(float time_step) {
 	auto transform = player->getComponent<Transform>();
-	float cur_dir = transform->getRotation();
-	cur_dir = atan2f(sin(cur_dir), cos(cur_dir));
-	cur_dir = (cur_dir < 0.0f ? 2 * PI + cur_dir : cur_dir);
+	
+	float x_mov = 0.0f;
+	if (input.isKeyPressed(Input::Key::RIGHT))
+		x_mov += 200.0f * time_step;
+	if (input.isKeyPressed(Input::Key::LEFT))
+		x_mov -= 200.0f * time_step;
+	transform->moveX(x_mov);
 
-	float mov = 0.0f;
+	int i = (transform->getPosition()[0] + 800.0f) / box_size;
+	float tile_height = 0.0f;
+	if (i >= 0 && i <= 31)
+		tile_height = tile_heights[i] * box_size + box_size * 0.5f - 450.0f;
+	
+	float jump_height = 50.0f;
+	float jump_time = 0.5;
 
-	if (abs(req_dir - cur_dir) > PI * 0.05f) {
-		if (abs(req_dir - cur_dir) <= PI) {
-			if (req_dir - cur_dir > 0.0f)
-				mov = 1.0f;
-			else
-				mov = -1.0f;
+	float acc = (-2 * jump_height) / (jump_time * jump_time / 4);
+	float u_y = -acc * jump_time / 2;
+
+	if (grounded) {
+		if ( input.isKeyPressed(Input::Key::UP)) {
+			grounded = false;
+			y_speed = u_y;
+		} else if (transform->getPosition()[1] < tile_height) {
+			grounded = false;
+			y_speed = 0.0f;
+		} else {
+			y_speed = 0.0f;
 		}
-		else {
-			if (req_dir - cur_dir > 0.0f)
-				mov = -1.0f;
-			else
-				mov = 1.0f;
-		}
+	} else {
+		y_speed += acc * time_step;
+		y_speed = std::max(-u_y, y_speed);
 	}
 
-	cur_dir += mov * PI * 0.5 * time_step;
-	transform->setRot(cur_dir);
-	transform->move(speed * cos(cur_dir + PI * 0.5f) * time_step, speed * sin(cur_dir + PI * 0.5f) * time_step);
-}
+	float prev_y = transform->getPosition()[1];
+	float next_y = prev_y + y_speed * time_step;
 
-inline void SceneGame::update_enemy(float time_step) {
-	vec<2> p_pos = player->getComponent<Transform>()->getPosition();
-	for (auto pair: enemies) {
-		auto enemy = pair.first;
-		auto& stat = pair.second;
-		auto e_transform = enemy->getComponent<Transform>();
-		vec<2> e_pos = e_transform->getPosition();
-		float delta_x = p_pos[0] - e_pos[0];
-		float delta_y = p_pos[1] - e_pos[1];
-		float dir = atan2f(delta_y, delta_x);
-		float x_mov = stat.speed * cos(dir) * time_step;
-		float y_mov = stat.speed * sin(dir) * time_step;
-
-		float dist = sqrtf(powf(delta_x, 2) + powf(delta_y, 2));
-		if (dist > stat.speed * time_step) {
-			e_transform->move(x_mov, y_mov);
-			e_transform->setRot(dir + PI * 0.5f);
-		}
-
-		stat.cooldown -= time_step;
-		if (stat.cooldown <= 0.0f) {
-			fireEnemyBullet(pair);
-			stat.resetCooldown();
-			
-		}
+	if (grounded && next_y > tile_height) {
+		grounded = false;
+	} else if (!grounded && next_y < tile_height && prev_y > tile_height) {
+		grounded = true;
+		next_y = tile_height;
 	}
-}
+	transform->setY(next_y);
 
-inline void SceneGame::updateEnemyBullet(float time_step) {
-	auto check = [this, &time_step](std::pair<std::shared_ptr<GameObject>, BulletStats> pair) {
-		auto bullet = pair.first;
-		auto stat = pair.second;
-		auto transform = bullet->transform;
-		auto angle = transform->getRotation();
-		vec<2> delta = vec<2>(cosf(angle - PI * 0.5f), sin(angle - PI * 0.5f)) * stat.speed * time_step;
-		transform->move(delta);
-		auto pos = transform->getPosition();
-		if (pos[0] < -800.0f || pos[0] > 800.0f || pos[1] < -450.0f || pos[1] > 450.0f) {
-			transform->setPos({ 1600.0f, 900.0f });
-			transform->setScale({ 0.0f, 0.0f });
-			inactive_objects.push_back(bullet);
-			return true;
-		}
-		return false;
-	};
-	std::remove_if(e_bullets.begin(), e_bullets.end(), check);
-}
-
-inline void SceneGame::end() {
-}
-
-inline EnemyStats::EnemyStats(int seed) :
-	type(seed % 5), health(0.0f), cooldown(0.0f), speed(0.0f), attack(0.0f) {
-	switch (type)
-	{
-	case 0:
-		health = 100.0f;
-		speed = 150.0f;
-		attack = 50.0f;
-		break;
-	case 1:
-		health = 200.0f;
-		speed = 200.0f;
-		attack = 30.0f;
-		break;
-	case 2:
-		health = 50.0f;
-		speed = 50.0f;
-		attack = 150.0f;
-		break;
-	case 3:
-		health = 150.0f;
-		speed = 150.0f;
-		attack = 60.0f;
-		break;
-	case 4:
-		health = 120.0f;
-		speed = 150.0f;
-		attack = 80.0f;
-		break;
+	if (transform->getPosition()[1] < -450.0f ||transform->getPosition()[0] < -800.0f || transform->getPosition()[0] > 800.0f ) {
+		transform->setPos(-15.5f * box_size, -5.0f * box_size);
+		grounded = false;
+		y_speed = 0.0f;
 	}
-	resetCooldown();
+
+	if (grounded && x_mov != 0.0f)
+		player->getComponent<Animation>()->setAnimation("move");
+	else
+		player->getComponent<Animation>()->setAnimation("idle");
 }
 
-inline EnemyStats::EnemyStats(const EnemyStats& other) :
-	health(other.health), speed(other.speed),
-	cooldown(other.cooldown), attack(other.attack), type(other.type){}
+void SceneGame::updateEnemies(float time_step) {
 
-inline void EnemyStats::resetCooldown() {
-	switch (type) {
-	case 0:
-		cooldown = 3.0f;
-		break;
-	case 1:
-		cooldown = 5.0f;
-		break;
-	case 2:
-		cooldown = 3.0f;
-		break;
-	case 3:
-		cooldown = 4.0f;
-		break;
-	case 4:
-		cooldown = 4.0f;
-		break;
-	}
 }
 
 #endif // !SCENE_GAME_HPP
